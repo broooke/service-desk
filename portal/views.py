1e0886f76da105ef77cb8766916006ab13f01f0e
broooke <9060826518ng@gmail.com>
2021-04-21 15:19:45 +0300

    dep

--- portal/views.py
+++ portal/views.py
@@ -26,6 +26,7 @@ class ApplicationsView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        print(self.request.META)
         x_forwarded_for = self.request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
             ip = x_forwarded_for.split(',')[0]
